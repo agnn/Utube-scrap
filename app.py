64dc9213a6e4de798b9e8baf16eef5274c8c2641
agnn <agnik.009@gmail.com>
2022-09-11 19:51:30 +0530

    runtime

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ async def scrap_video_content(url):
     session._browser = browser
     r = await session.get(url)
 
-    await r.html.arender(sleep=1, keep_page=True, scrolldown=30,timeout=30000)
+    await r.html.arender(sleep=1, keep_page=True, scrolldown=4,timeout=30)
 
     views = r.html.find('span.view-count.style-scope.ytd-video-view-count-renderer')
     likes = r.html.find('yt-formatted-string#text.style-scope.ytd-toggle-button-renderer.style-text')
@@ -150,7 +150,7 @@ async def scrap_video_url(u):
     session._browser = browser
     r = await session.get(u)
 
-    await r.html.arender(sleep=1,keep_page=True, scrolldown=4,timeout=30000)
+    await r.html.arender(sleep=1,keep_page=True, scrolldown=4,timeout=30)
 
     video_titles = ([item.text for item in r.html.find('#video-title')])
 
